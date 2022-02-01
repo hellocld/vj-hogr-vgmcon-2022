ac0ec7972e3e26213b861df53e7f65b965c0f43b
Christopher Ray Langford <chris@hellocld.com>
2022-01-31 23:15:21 -0500

    Vectorman anims, initial DiscCylinder

--- Scenes/MiniSculptures/VectormanItem.gd
+++ Scenes/MiniSculptures/VectormanItem.gd
@@ -4,17 +4,22 @@ export(int) var base_static_idx := 3
 
 onready var _base := $Base
 onready var _orb := $Orb
-onready var _spikes1 := $Orb/Spikes001
-onready var _spikes2 := $Orb/Spikes002
-onready var _spikes3 := $Orb/Spikes003
+onready var _spikes1 := $Orb/SpikesCenter001/Spikes001
+onready var _spikes2 := $Orb/SpikesCenter002/Spikes002
+onready var _spikes3 := $Orb/SpikesCenter003/Spikes003
+
+var state_machine : AnimationNodeStateMachinePlayback
+var _expand_count := 0
 
 var _base_mat
 
 
 func _ready() -> void:
-	_base_mat = _base.get_active_material(0)
+	_base_mat = _base.mesh.surface_get_material(0)
 	SpectrumAnalyzer.connect("sample_triggered", self, "_enable_tv_static")
+	SpectrumAnalyzer.connect("sample_triggered", self, "_toggle_spikes_state")
 	#SpectrumAnalyzer.connect("sample_detriggered", self, "_disable_tv_static")
+	state_machine = $AnimationTree["parameters/playback"]
 
 func _process(delta: float) -> void:
 	_update_tv_static()
@@ -44,3 +49,16 @@ func _update_spikes() -> void:
 	_spikes1.scale = Vector3.ONE * (SpectrumAnalyzer.get_sample(6) + 0.8)
 	_spikes2.scale = Vector3.ONE * (SpectrumAnalyzer.get_sample(6) + 0.8)
 	_spikes3.scale = Vector3.ONE * (SpectrumAnalyzer.get_sample(6) + 0.8)
+
+
+func _toggle_spikes_state(idx) -> void:
+	if idx != 8:
+		pass
+	_expand_count += 1
+	if _expand_count > 256:
+		if state_machine.get_current_node() == "Expand":
+			state_machine.travel("Collapse")
+		else:
+			state_machine.travel("Expand")
+		_expand_count = 0
+
