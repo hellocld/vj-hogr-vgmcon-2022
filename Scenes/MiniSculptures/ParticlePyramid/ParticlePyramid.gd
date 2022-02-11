ed403fda392af57fc699a39341667a3fbf64f748
Christopher Ray Langford <chris@hellocld.com>
2022-02-11 17:08:00 -0500

    Landscape for MiniSculptures

---
+++ Scenes/MiniSculptures/ParticlePyramid/ParticlePyramid.gd
@@ -0,0 +1,13 @@
+extends Spatial
+
+onready var _anim_tree_node = $AnimationTree
+var _anim_tree : AnimationNodeStateMachinePlayback
+
+
+func _ready() -> void:
+	_anim_tree = _anim_tree_node["parameters/playback"] as AnimationNodeStateMachinePlayback
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("controller_b"):
+		_anim_tree_node["parameters/conditions/expand"] = !_anim_tree_node["parameters/conditions/expand"]
+		_anim_tree_node["parameters/conditions/contract"] = !_anim_tree_node["parameters/conditions/contract"]
