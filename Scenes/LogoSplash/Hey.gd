053da9ac0260bb36305060d50ab9a0fbf22684a5
Christopher Ray Langford <chris@hellocld.com>
2022-03-20 11:05:56 -0400

    HEY color and border

--- Scenes/LogoSplash/Hey.gd
+++ Scenes/LogoSplash/Hey.gd
@@ -1,9 +1,10 @@
 extends ViewportContainer
 
 onready var anim = $Viewport/AnimationPlayer
-
+onready var sprite = $Viewport/Sprite
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Hey"):
+		sprite.self_modulate = Color.from_hsv(rand_range(0.0, 1.0), 1.0, 1.0, 1.0)
 		anim.stop()
 		anim.play("New Anim")
